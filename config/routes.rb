d88377943fcfba25331f68e2f23c2ee5c5832825
Krzysztof Witczak <kwitczak19@gmail.com>
2017-07-23 19:34:08 +0200

    Added posts model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'home_page#index'
   get 'home_page', to: 'home_page#index'
+
+  get 'admin', to: 'admin#index'
+  get 'blog', to: 'posts#index', as: :blog
+
+  resources :posts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
